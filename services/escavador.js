e69ac147514f9ce48186a67a9059f2df1f5e7166
Alexandrexicara <santossilvacelio73@gmail.com>
2026-06-09 18:40:14 -0300

    commit index

--- services/escavador.js
+++ services/escavador.js
@@ -5,6 +5,7 @@ const BASE = 'https://api.escavador.com/api/v2';
 
 const headers = {
     'Authorization': `Bearer ${API_KEY}`,
+    'Accept': 'application/json',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
 };
 
